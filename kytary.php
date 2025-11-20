b0915f94b1d8c611c62b81cc8bc9134d4be0ffe0
strasser <strasserfilipos@gmail.com>
2025-11-20 10:21:39 +0100

    3

--- kytary.php
+++ kytary.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js"></script>
+
 </head>
 
 <body>
