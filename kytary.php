124ea6333614df0eb99a62ece4e158eef3a07fe8
MagorStevie <svatikrejsa2008@gmail.com>
2025-11-22 16:23:14 +0100

    Pridani jsonu a jeho funkcnost

--- kytary.php
+++ kytary.php
@@ -13,11 +13,15 @@
 <body>
 
     <?php
-    include("includes/header.php")
+    include("includes/header.php");
+        
+    include("includes/side.php");
+
+    $json = file_get_contents('products.json');
+    $products = json_decode($json, true)
         ?>
-    <?php
-    include("includes/side.php")
-        ?>
+
+
     <div class="hero">
         <div class="hero__right">
             <h1>Kytary</h1>
@@ -29,22 +33,27 @@
                 <a class="type__element">Efekty na kytary</a>
             </div>
             <div class="shop">
-                <a href="shop__item.php" class="shop__element">
-                    <img class="shop__image" src="images/shop__images/guitar.jpg" alt="">
 
-                    <h2>Produkt</h2>
-                    <p>cena</p>
+<?php
+      foreach ($products as $product) {
+        $popis = (mb_strlen($product['popis'], 'UTF-8') > 50) ? mb_substr($product['popis'], 0, 50, 'UTF-8') . '...' : $product['popis'];
+        echo('
 
-                </a>
-                <div class="shop__element">.</div>
-                <div class="shop__element">.</div>
+                <a href="shop__item.php?id=' . $product['id'] . '" class="shop__element">
+                    <img class="shop__image" src="images/shop__images/' . $product['obrazek'] .'" alt="">
+                    <h2> ' . $product['nazev'] . ' </h2>
+                    <p> ' . $product['cena'] . ' </p>
+                </a>');
+    }
+?>
             </div>
         </div>
     </div>
 
-    <?php
+<?php
     include("includes/footer.php")
-        ?>
+?>
+
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"></script>
     <script src="script.js"></script>
 </body>
--- shop__item.php
+++ shop__item.php
@@ -1,3 +1,27 @@
+<?php
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $json = file_get_contents('products.json');
+    $products = json_decode($json, true);
+
+    foreach ($products as $product) {
+        if ($product['id'] == $id) {
+            $nazev = $product['nazev'];
+            $popis = $product['popis'];
+            $cena = $product['cena'];
+            $obrazek = $product['obrazek'];
+            $recenze = $product['recenze'];
+            break;
+        }
+    }
+} else {
+    header('location: kytary.php');
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="cs">
 
@@ -13,36 +37,40 @@
 <body>
 
     <?php
-    include("includes/header.php")
-        ?>
-    <?php
+    include("includes/header.php");
+
     include("includes/side.php")
         ?>
+
+
     <div class="hero">
         <div class="hero__right">
+
+    <?php 
+        echo('        
             <div class="product">
                 <div class="product__element">
-
                     <div class="product__left">
-                        <img class="shop__image" src="images/shop__images/guitar.jpg" alt="">
+                        <img class="shop__image" src="images/shop__images/' . $obrazek . '" alt="">
                     </div>
                     <div class="product__right">
                         <div class="product__info">
-                            <h1>Produkt</h1>
-                            <p>popis</p>
+                            <h1> ' . $nazev . ' </h1>
+                            <p> ' . $popis . '</p>
                         </div>
                         <div class="product__price">
-
-                            <h2>cena</h2>
+                            <h2> ' . $cena . '</h2>
                             <button class="buy">Přidat do košíku</button>
                         </div>
                     </div>
                 </div>
                 <div class="reviews">
-                    <h2>Recenze</h2>
-                    <div class="reviews__element">...</div>
+                    <h2> Recenze </h2>
+                    <div class="reviews__element"> ' . $recenze . ' </div>
                 </div>
-            </div>
+            </div>')
+    ?>
+
         </div>
     </div>
 
